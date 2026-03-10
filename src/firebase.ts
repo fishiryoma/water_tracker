e8c86800b9eeb3b7b7e507999e725cdc3bbe02a3
wanyuhuang <wanyu@meta-energy.com.tw>
2026-03-10 16:56:45 +0800

    使用env保存敏感資料

--- src/firebase.ts
+++ src/firebase.ts
@@ -8,14 +8,14 @@ import { getDatabase } from 'firebase/database'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyDiTXgzOEu0kQ8t37prqGA-alI-cKN7RkU',
-  authDomain: 'water-record.firebaseapp.com',
-  databaseURL: 'https://water-record-default-rtdb.asia-southeast1.firebasedatabase.app',
-  projectId: 'water-record',
-  storageBucket: 'water-record.firebasestorage.app',
-  messagingSenderId: '260404007186',
-  appId: '1:260404007186:web:d75c36dc3bf91e824723cb',
-  measurementId: 'G-74YKJS7W58',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 }
 
 // Initialize Firebase
